88be2b817ec26baa3ece90d7cedd39fbee22be1e
Wellington <wsilva@usp.br>
2016-07-28 04:56:15 -0300

    added redis to persist like qtde

--- src/app.py
+++ src/app.py
@@ -1,17 +1,27 @@
 # -*- coding: UTF-8 -*-
 from flask import Flask, render_template, request
-import socket
+import os, redis, socket
 
 app = Flask(__name__)
+app.redis = redis.StrictRedis(host=os.getenv('REDIS_HOST', 'cache'), 
+            port=os.getenv('REDIS_PORT', 6379), 
+            db=0)
 
 
-@app.route("/")
+@app.route("/", methods = ['POST', 'GET'])
 def main():
+
 
     hostname=socket.gethostname()
     ipaddr=socket.gethostbyname(hostname)
+    qtde = app.redis.get("curtidas")
+    curtidas = int(qtde or 0)
 
-    return render_template('index.html', ipaddr=ipaddr, hostname=hostname)
+    if (request.method == 'POST'):
+        curtidas += 1
+        app.redis.set("curtidas", curtidas)
+
+    return render_template('index.html', ipaddr=ipaddr, hostname=hostname, curtidas=curtidas)
 
 if __name__ == "__main__":
     app.debug = True
